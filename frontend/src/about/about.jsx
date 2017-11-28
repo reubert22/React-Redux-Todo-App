9486e99005dd49cf4b8ccde50f047d003f066a9a
reubertbarbosa <reubertfernandes@gmail.com>
2017-11-28 20:18:08 -0200

    including Form and List on

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Missão</h2>
+        <p>Some paragraph</p>
+        <h2>Visão</h2>
+        <p>Some paragraph</p>
+        <h2>Valores</h2>
+        <p>Some paragraph</p>
     </div>
 )
---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default props => (
+    <div role='form' className='todoForm'>
+        <div className='col-xs-12 col-sm-9 col-md-10'>
+            <input id='description' className='form-control'
+            placeholder='Adicione uma tarefa'></input>
+        </div>
+
+        <div className='col-xs-12 col-sm-3 col-md-2'>
+            <button className='btn btn-primary'>
+                <i className='fa fa-plus'></i>
+            </button>
+        </div>
+    </div>
+)
