5616d8e96a8ea390b169c345149b825c1bf1fc51
reubertbarbosa <reubertfernandes@gmail.com>
2017-11-28 19:55:34 -0200

    frontend implementations

---
+++ frontend/src/template/menu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default props => (
+    <nav className='navbar navbar-inverse bg-inverse'>
+        <div className='container'>
+            <div className='navbar-header'>
+                <a className='navbar-brand' href='#'>
+                    <i className='fa fa-calendar-check-o'></i> TodoApp
+                </a>
+            </div>
+        </div>
+
+    </nav>
+)
