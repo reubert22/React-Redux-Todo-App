c2092f1020f013f6e86df59ac9780b2d833e1c35
reubertbarbosa <reubertfernandes@gmail.com>
2017-11-30 00:36:08 -0200

    added add-plus btn and integrated on todo/todoForm

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,30 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props){
+        super(props)
+        this.state = {description: '', list: []}
+
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleAdd() {
+        console.log(this.state.description)
+    }
+
+    handleChange() {
+        this.setState({...this.state, description: e.target.value})
+    }
+
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm />
+                <TodoForm 
+                    description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd}/>
                 <TodoList />
             </div>
         )
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Grid from './grid'
+import IconButton from '../template/iconButton'
 
 export default props => (
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
             <input id='description' className='form-control'
-            placeholder='Adicione uma tarefa'></input>
+                placeholder='Adicione uma tarefa'
+                onChange={props.handleChange}
+                value={props.description}></input>
         </Grid>
 
         <Grid cols='12 3 2'>
-            <button className='btn btn-primary'>
-                <i className='fa fa-plus'></i>
-            </button>
+            <IconButton style='primary' icon='plus'
+                onClick={props.handleAdd}></IconButton>
         </Grid>
     </div>
 )
