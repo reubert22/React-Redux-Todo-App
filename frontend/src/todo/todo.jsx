375d117543fcb2fca74167c4432d9d9baee99417
reubertbarbosa <reubertfernandes@gmail.com>
2017-11-30 00:40:11 -0200

    initial integration w/ backend database

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
     constructor(props){
@@ -13,7 +17,10 @@ export default class Todo extends Component {
     }
 
     handleAdd() {
-        console.log(this.state.description)
+        const description = this.state.description
+        
+        axios.post(URL, {description})
+            .then(resp => console.log('Funcionou!'))
     }
 
     handleChange() {
