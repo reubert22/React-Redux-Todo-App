4f0ba981e5d0386561e470dea0b0337c29906a9f
reubertbarbosa <reubertfernandes@gmail.com>
2017-12-01 02:06:05 -0200

    correction on menu

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -8,6 +8,13 @@ export default props => (
                     <i className='fa fa-calendar-check-o'></i> TodoApp
                 </a>
             </div>
+
+            <div id="navbar" className='navbar-collapse collapse'>
+                <ul className="nav navbar-nav">
+                    <li><a href="#/todos">Tarefas</a></li>
+                    <li><a href="#/about">Sobre</a></li>
+                </ul>
+            </div>
         </div>
 
     </nav>
