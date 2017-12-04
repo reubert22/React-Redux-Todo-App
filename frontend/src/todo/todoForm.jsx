313a1755c6070ce7d7e98f6f79f4afccccb41169
reubertbarbosa <reubertfernandes@gmail.com>
2017-12-04 18:21:55 -0200

    integration w/ Redux 1.2 - TodoForm

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import Grid from './grid'
 import IconButton from '../template/iconButton'
 
-export default props => {
+const TodoForm = props => {
     
     const keyHandler = (e) => {
         if(e.key === 'Enter'){
@@ -36,3 +38,6 @@ export default props => {
         </div>
     )
 }
+
+const mapStateToProps = state => ({description: state.todo.description})
+export default connect(mapStateToProps)(TodoForm)
